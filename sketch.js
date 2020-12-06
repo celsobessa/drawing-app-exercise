3b9fe99bbbdff70f04345d3c4efb1fafbb85ee59
Celso Bessa <celso@celsobessa.com.br>
2020-12-06 19:10:02 -0300

    add mousePressed and keyPressed; add utilities function rgbaRandom and randomNumber

--- sketch.js
+++ sketch.js
@@ -1,13 +1,32 @@
 function setup()
 {
 	createCanvas(600, 600);
+	fill(255, 255, 255);
 
 }
 
 function draw()
 {
 	background(0, 0, 0);
-	fill(255, 255, 255);
 	ellipse(mouseX, mouseY, 100, 100);
 
+}
+
+function mousePressed(){
+    fill(rgbaRandom(),rgbaRandom(),rgbaRandom());
+}
+
+function keyPressed(){
+    fill(255, 255, 255);
+}
+
+function rgbaRandom() {
+    return randomNumber(255,0);
+}
+function randomNumber(max = 1, min = 0) {
+  if(min >= max) {
+    return max;
+  }
+
+  return Math.floor(Math.random() * (max - min) + min);
 }
